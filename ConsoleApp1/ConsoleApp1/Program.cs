4cd5e7c04b7ec71ed4eb366af56e63a85944ffa2
mkeshilbaev <m.keshilbaev@gmail.com>
2018-01-15 20:10:29 +0600

    4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] arr = s.Split(' ');
+
+            foreach(string i in arr)
+            {
+                Console.Write(int.Parse(i) + " ");
+            }
+        }
+    }
+}
