b7ef82d9bf4a15cda7f7caadf48797b6c286a6d4
mkeshilbaev <m.keshilbaev@gmail.com>
2018-01-16 23:58:52 +0600

    CIRCLE DONE

---
+++ Lab 1/Circle/Circle/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Circle
+    {
+        public double radius;
+        public double area;
+        public double diameter;
+        public double circumference;
+
+        public Circle (double r)
+        {
+            radius = r;
+            FindArea();
+            FindDiameter();
+            FindCircumference();
+        }
+        public void FindArea()
+        {
+            area = Math.PI * radius * radius;
+        }
+        public void FindDiameter()
+        {
+            diameter = radius * 2;
+        }
+        public void FindCircumference()
+        {
+            circumference = 2 * Math.PI * radius;
+        }
+        public override string ToString()
+        {
+            return "Area = " + area + "\nDiameter = " + diameter + "\nCircumference = " + circumference;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            Circle c = new Circle(radius);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
