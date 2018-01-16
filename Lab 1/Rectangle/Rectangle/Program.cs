4cb3a6c9dbcfc7264d336c7cbee5514d0f3b83ec
mkeshilbaev <m.keshilbaev@gmail.com>
2018-01-16 23:46:26 +0600

    Problem Reactangle

---
+++ Lab 1/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+            public double width;
+        public double height;
+        public double area;
+        public double perimenter;
+
+        public Rectangle (double w, double h)
+        {
+            width = w;
+            height = h;
+            FindArea();
+            FindPerimeter();
+        }
+        public void FindArea()
+        {
+            area = width * height;
+        }
+        public void FindPerimeter()
+        {
+            perimenter = (width + height) * 2;
+        }
+        public override string ToString()
+        {
+            return "Area = " + area + "\nPerimeter = " + perimenter;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Rectangle m = new Rectangle(width, height);
+            Console.WriteLine(m);
+            Console.ReadKey();
+        }
+    }
+}
