2784da5d3b915b7f70e6ae56639f592b71b42b4d
mkeshilbaev <m.keshilbaev@gmail.com>
2018-01-21 22:55:50 +0600

    Max and Min from input.txt  done

---
+++ Lab 2/max and min from file/max and min from file/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace max_and_min_from_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader SR = new StreamReader("d:\\input.txt");
+            string s = SR.ReadLine();
+            string[] arr = s.Split();
+            int[] a = new int[arr.Length];
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                a[i] = int.Parse(arr[i]);
+            }
+
+            int mx = a[0];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (mx < a[i])
+                {
+                    mx = a[i];
+                }
+            }
+            Console.WriteLine(mx);
+
+            int mn = a[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (mn > a[i])
+                {
+
+                }
+            }
+            Console.WriteLine(mn);
+
+            //SR.Close();
+
+            //foreach (string sOutput in arr)
+                //Console.WriteLine(sOutput);
+            Console.ReadLine();
+        }
+    }
+}
