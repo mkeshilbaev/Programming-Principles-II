03fd90a300785358ced02f00ae2a82f6cf228749
mkeshilbaev <m.keshilbaev@gmail.com>
2018-01-28 16:44:41 +0600

    File Manager Done

---
+++ Lab 3/FileManager/FileManager/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileManager
+{
+    class Program
+    {
+        static int CONSOLE_SIZE = 27;
+        static void ShowDirectoryInfo(DirectoryInfo directory, int cursor, int first)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.CursorVisible = false;
+            FileSystemInfo[] fss = directory.GetFileSystemInfos();
+            int index = 0;
+
+            foreach (FileSystemInfo f in fss)
+            {
+                if (index == cursor)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                }
+
+                else if (f.GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+
+                if (index >= first && index <= first + CONSOLE_SIZE)
+                    Console.WriteLine(f.Name);
+                index++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\");
+            int first = 0;
+            int cursor = 0;
+            ShowDirectoryInfo(directory, cursor, first);
+            int n = directory.GetFileSystemInfos().Length;
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    cursor--;
+
+                    if (cursor < 0)
+                    {
+                        cursor = n - 1;
+                        first = cursor - CONSOLE_SIZE;
+                    }
+
+                    if (cursor < first)
+                    {
+                        first--;
+                    }
+                }
+
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    cursor++;
+                    if (cursor == n)
+                    {
+                        cursor = 0;
+                        first = 0;
+                    }
+                    if (cursor > first + CONSOLE_SIZE)
+                    {
+                        first++;
+                    }
+                }
+               
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    if (directory.GetFileSystemInfos()[cursor].GetType() == typeof(DirectoryInfo))
+                    {
+                        directory = (DirectoryInfo)directory.GetFileSystemInfos()[cursor];
+                        cursor = 0;
+                        n = directory.GetFileSystemInfos().Length;
+                        first = 0;
+                    }
+
+                    else
+                    {
+                        StreamReader sr = new StreamReader(directory.GetFileSystemInfos()[cursor].FullName);
+                        string s = sr.ReadToEnd();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Clear();
+                        Console.WriteLine(s);
+                        Console.ReadKey();
+                    }
+                }
+
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    if (directory.Parent != null)
+                    {
+                        directory = directory.Parent;
+                        cursor = 0;
+                        n = directory.GetFileSystemInfos().Length;
+                        first = 0;
+                    }
+
+                    else
+                        break;        
+                }
+                ShowDirectoryInfo(directory, cursor, first);
+            }
+        }
+    }
+}
--- Lab 3/FileManager/FileManager/Properties/AssemblyInfo.cs
+++ Lab 3/FileManager/FileManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("FileManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("FileManager")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("47b52a2c-7377-435f-8761-c9013cb2ee7e")]
+[assembly: Guid("4fd52200-f8b8-416e-9cf5-81dc64bf73dc")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
