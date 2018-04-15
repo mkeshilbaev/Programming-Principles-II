d1430742841537f62fe120211bb964574315a36d
mkeshilbaev <m.keshilbaev@gmail.com>
2018-04-15 20:04:15 +0600

    Asteroid

--- 2 attestaion/Asteroid/Asteroid/Form1.Designer.cs
+++ 2 attestaion/Asteroid/Asteroid/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(1282, 753);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
--- 2 attestaion/Asteroid/Asteroid/Form1.cs
+++ 2 attestaion/Asteroid/Asteroid/Form1.cs
@@ -12,35 +12,26 @@ namespace Asteroid
 {
     public partial class Form1 : Form
     {
-        /*Point firstPoint = default(Point);
-        Point secondPoint = default(Point);
-        Graphics gfx = default(Graphics);
-        Pen pen = new Pen(Color.Red, 3);*/
-        Graphics gr;
-        Pen myPen = new Pen(Color.Black, 1);
-        float GameWidth = 1000;
-        float GameHeight = 600;
+        Graphics g;
+        Pen Pen = new Pen(Color.Black, 1);
+        float GameWidth = 1300;
+        float GameHeight = 800;
 
         public Form1()
         {
             InitializeComponent();
-            gr = CreateGraphics();
-
-            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            myPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
-            myPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            g = CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            Pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //
             // Setting up background
-            //
             DrawBackground(GameWidth, GameHeight);
 
-            //
             // Drawing some stars
-            //
             DrawStar(50, 70, 14);
             DrawStar(350, 65, 14);
             DrawStar(650, 125, 14);
@@ -50,66 +41,52 @@ namespace Asteroid
             DrawStar(720, 410, 14);
             DrawStar(900, 380, 14);
 
-            //
             // Setting up Information table
-            //
             DrawInfoTable(700, 30, 250, 40);
 
-            //
             // Drawing SpaceShip
-            //
             DrawSpaceship(450, 250);
 
-            // 
             // Drawing some asteroids
-            //
             DrawAsteroid(150, 220);
             DrawAsteroid(270, 350);
             DrawAsteroid(850, 170);
             DrawAsteroid(650, 520);
 
-            //
             // Drawing bullet
-            //
             DrawBullet(480, 190);
-
-
-
-
         }
 
         private void DrawBackground(float x, float y)
         {
             Brush myBrush = new SolidBrush(Color.BlueViolet);
             RectangleF BackgroundRectangle = new RectangleF(new Point(0, 0), new SizeF(x, y));
-            gr.FillRectangle(myBrush, BackgroundRectangle);
+            g.FillRectangle(myBrush, BackgroundRectangle);
 
-            myPen.Width = 25;
-            gr.DrawRectangle(myPen, 0, 0, x, y);
+            Pen.Width = 25;
+            g.DrawRectangle(Pen, 0, 0, x, y);
         }
+
         private void DrawInfoTable(float x, float y, float w, float h)
         {
             RectangleF InfoTable = new RectangleF(new PointF(x, y), new SizeF(w, h));
-            gr.FillRectangle(Brushes.White, InfoTable);
+            g.FillRectangle(Brushes.White, InfoTable);
 
-            myPen.Width = 2;
-            myPen.Color = Color.Orange;
-            gr.DrawRectangle(myPen, x, y, w, h);
+            Pen.Width = 2;
+            Pen.Color = Color.Orange;
+            g.DrawRectangle(Pen, x, y, w, h);
 
             string DrawString = "Level: 1 Score: 200 Live: ***";
             Font DrawFont = new Font("Arial", 14);
             Brush DrawBrush = new SolidBrush(Color.Red);
             PointF DrawPoint = new PointF(x + 2, y + 9);
-            gr.DrawString(DrawString, DrawFont, DrawBrush, DrawPoint);
-
+            g.DrawString(DrawString, DrawFont, DrawBrush, DrawPoint);
         }
 
         private void DrawStar(float x, float y, float size)
         {
             RectangleF star = new RectangleF(new PointF(x, y), new SizeF(size * 3, size * 3));
-            gr.FillEllipse(Brushes.White, star);
-
-
+            g.FillEllipse(Brushes.White, star);
         }
 
         private void DrawSpaceship(float x, float y)
@@ -122,10 +99,10 @@ namespace Asteroid
                 new PointF(x + 60, y + 70),
                 new PointF(x, y + 50) };
 
-            gr.FillPolygon(Brushes.Yellow, spaceshipPoints);
+            g.FillPolygon(Brushes.Yellow, spaceshipPoints);
             DrawGun(x + 50, y + 24);
-
         }
+
         private void DrawGun(float x, float y)
         {
             PointF[] gunPoints = {
@@ -136,8 +113,7 @@ namespace Asteroid
                 new PointF(x + 20, y),
                 new PointF(x + 20, y + 22),
                 new PointF(x, y + 22)};
-
-            gr.FillPolygon(Brushes.Green, gunPoints);
+            g.FillPolygon(Brushes.Green, gunPoints);
         }
 
         private void DrawAsteroid(float x, float y)
@@ -146,21 +122,16 @@ namespace Asteroid
             {new PointF(x,y),
             new PointF(x + 35, y - 50),
             new PointF(x + 70, y)
-
             };
 
             PointF[] trianglePoints2 =
             {new PointF(x, y - 30),
             new PointF(x + 70, y - 30),
             new PointF(x + 35, y + 20)
-
             };
 
-            gr.FillPolygon(Brushes.Red, trianglePoints1);
-            gr.FillPolygon(Brushes.Red, trianglePoints2);
-
-
-
+            g.FillPolygon(Brushes.Red, trianglePoints1);
+            g.FillPolygon(Brushes.Red, trianglePoints2);
         }
 
         private void DrawBullet(float x, float y)
@@ -177,7 +148,7 @@ namespace Asteroid
                 new PointF(x + 15, y + 5)
             };
 
-            gr.FillPolygon(Brushes.LightGreen, bulletPoints);
+            g.FillPolygon(Brushes.LightGreen, bulletPoints);
         }
     }
 }
